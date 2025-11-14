79352fd3c35636d427ed8b741bd46a2bcc9624fe
Elesar De La Vega <elesarvega@gmail.com>
2025-11-14 15:33:53 +0100

    chore: :construction: add new remix and mashups etc

--- src/data/musicData.ts
+++ src/data/musicData.ts
@@ -1,6 +1,7 @@
-import ILikeTheSun from '../assets/songs/ILikeTheSun.webp'
-import WhatchaSayGirl from '../assets/songs/WhatchaSayGirl.webp'
-import KassandraFeeling from '../assets/songs/KassandraFeeling.webp'
+import ILikeTheSun from '../assets/songs/mashup/ILikeTheSun.webp'
+import WhatchaSayGirl from '../assets/songs/mashup/WhatchaSayGirl.webp'
+import KassandraFeeling from '../assets/songs/mashup/KassandraFeeling.webp'
+import LoversDavidGuettaXAvicii from '../assets/songs/mashup/LoversDavidGuettaXAvicii.webp';
 
 export const mashups = [
   {
@@ -21,12 +22,24 @@ export const mashups = [
     cover: WhatchaSayGirl,
     link: 'https://soundcloud.com/elesardelavega/whatcha-say-girl-mashup-elesardelavega',
   },
-]
+  {
+    title: 'Lovers On The Love David Guetta X Avicii',
+    artist: 'Elesar De La Vega - Electronica',
+    cover: LoversDavidGuettaXAvicii,
+    link: 'https://soundcloud.com/elesardelavega/lovers-on-the-love-david-guetta-x-avicii-mashup-elesardelavega',
+  },
+];
 
 import LaLuz from '../assets/songs/remix/LaLuz.webp'
 import PonleRemix from '../assets/songs/remix/PonleRemix.webp'
 import ProblemaRemix from '../assets/songs/remix/ProblemaRemix.webp'
 import TodoDeTi from '../assets/songs/remix/TodoDeTi.webp'
+import Dakiti from '../assets/songs/remix/Dákiti.webp'
+import Caramelo from '../assets/songs/remix/caramelo.webp'
+import Jeans from '../assets/songs/remix/jeans.webp'
+import Curiosidad from '../assets/songs/remix/laCuriosidad.webp'
+import Nina from '../assets/songs/remix/miNina.webp'
+import Bailando from '../assets/songs/remix/sigueloBailando.webp'
 
 export const remixes = [
   {
@@ -53,7 +66,43 @@ export const remixes = [
     cover: TodoDeTi,
     link: 'https://soundcloud.com/elesardelavega/rauw-alejandro-todo-de-ti-remix-urbanhouse-elesardelavega-filtrada',
   },
-]
+  {
+    title: 'Bad Bunny x Jhay Cortez - Dákiti',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Dakiti,
+    link: 'https://soundcloud.com/elesardelavega/bad-bunny-x-jhay-cortez-dakiti-remix-urban-tech-house-elesardelavega',
+  },
+  {
+    title: 'Ozuna - Caramelo',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Caramelo,
+    link: 'https://soundcloud.com/elesardelavega/ozuna-caramelo-remix-elesardelavega',
+  },
+  {
+    title: 'Justin Quiles - Jeans',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Jeans,
+    link: 'https://soundcloud.com/elesardelavega/justin-quiles-jeans-remix-elesardelavega',
+  },
+  {
+    title: 'Ozuna – Mi Niña',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Nina,
+    link: 'https://soundcloud.com/elesardelavega/ozuna-mi-nina-remix-elesardelavega',
+  },
+  {
+    title: 'Ozuna - Siguelo Bailando',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Bailando,
+    link: 'https://soundcloud.com/elesardelavega/ozuna-siguelo-bailando-remix-elesardelavega',
+  },
+  {
+    title: 'Jay Wheeler, Myke Towers – La Curiosidad',
+    artist: 'Elesar De La Vega - Urban & Tech House',
+    cover: Curiosidad,
+    link: 'https://soundcloud.com/elesardelavega/jay-wheeler-myke-towers-la-curiosidad-remix-elesardelavega',
+  },
+];
 
 import sesionCrossfit from '../assets/songs/sesion/crossfit.webp';
 import sesionTechno from '../assets/songs/sesion/techno.webp'
