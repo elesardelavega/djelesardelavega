0169fecd35a9ef206dec03010d859eb379040223
Elesar De La Vega <elesarvega@gmail.com>
2025-11-18 11:02:43 +0100

    docs: :sparkles: ADD NEW FOTOS

--- src/data/media.ts
+++ src/data/media.ts
@@ -1,8 +1,15 @@
-
 import foto1 from '../assets/fotos/1.webp'
-import foto2 from '../assets/fotos/1n.webp'
 import foto3 from '../assets/fotos/2.webp'
 import foto4 from '../assets/fotos/3.webp'
+import foto5 from '../assets/fotos/4.webp'
+import foto6 from '../assets/fotos/5.webp'
+import foto7 from '../assets/fotos/6.webp'
+import foto8 from '../assets/fotos/7.webp'
+
+import foto2 from '../assets/fotos/1n.webp'
+import foto2n from '../assets/fotos/2n.webp'
+import foto3n from '../assets/fotos/3n.webp'
+import foto4n from '../assets/fotos/4n.webp'
 
 import logo1 from '../assets/logoBlancoBlanco.webp'
 import logo2 from '../assets/logoBlancoAzul.webp'
@@ -12,7 +19,7 @@ import logo4 from '../assets/logoNegro.webp'
 import fotosZip from '../assets/zip/FOTOS_ELESARDELAVEGA.zip'
 import logosZip from '../assets/zip/LOGOS_ELESARDELAVEGA.zip'
 
-export const fotos = [foto1, foto2, foto3, foto4]
+export const fotos = [foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8, foto2n, foto3n, foto4n]
 export const logos = [logo1, logo2, logo3, logo4]
 
 export const fotosZipUrl = fotosZip
